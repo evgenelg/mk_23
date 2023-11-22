cef409bf0735d190cf8d7e05d05a3450c73fd122
Елгазин Евгений <ese2003@yandex.ru>
2023-11-22 13:16:32 +0300

    НЕЗАКОНЧЕННАЯ ПРОГРАММА

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 
+#include <libopencm3/cm3/nvic.h>
+
 constexpr uint16_t PERIOD_MS{1000};
 
 void timer_setup (){
@@ -9,13 +11,16 @@ void timer_setup (){
     timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6)/PERIOD_MS - 1);
     timer_set_period(TIM6, PERIOD_MS - 1);
     
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
+
     timer_enable_counter(TIM6);
 }
 
 
 void gpio_setup () {
     rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);    
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11);    
 }
 
 
@@ -33,10 +38,6 @@ void blink_LED(){
 
 
 
-
-
-
-
 int main () {
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
@@ -47,5 +48,10 @@ while (true){
     blink_LED();
 }
 
+
+}
+
+void tim6_dac_isr () {
+    gpio_toggle(GPIOE, GPIO11);
 
 }
