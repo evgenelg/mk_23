7e22ecf95a3d4a592747333e9d1ed1677f2210a5
Елгазин Евгений <ese2003@yandex.ru>
2023-12-06 13:07:51 +0300

    l7

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
-#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/timer.h>
-
 #include <libopencm3/cm3/nvic.h>
 
-constexpr uint16_t PERIOD_MS{1000};
 
-void timer_setup (){
+constexpr uint16_t PRESCALER{100};
+constexpr uint16_t PERIOD_US{32000};
+
+
+
+void timer_setup() {
     rcc_periph_clock_enable(RCC_TIM6);
-    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6)/PERIOD_MS - 1);
-    timer_set_period(TIM6, PERIOD_MS - 1);
-    
+    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / PRESCALER - 1);
+    timer_set_period(TIM6, PERIOD_US - 1);
+
     timer_enable_irq(TIM6, TIM_DIER_UIE);
     nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
 
@@ -18,40 +21,24 @@ void timer_setup (){
 }
 
 
-void gpio_setup () {
+void gpio_setup() {
     rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11);    
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11);
 }
-
-
-void blink_LED(){
-          if(timer_get_counter(TIM6) < PERIOD_MS / 2) {
-            gpio_set(GPIOE, GPIO9);
-        }
-        else {
-            gpio_clear(GPIOE, GPIO9);
-        }  
-}
-
-
-
-
 
 
 int main () {
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
-timer_setup();
-gpio_setup();
+    timer_setup();
+    gpio_setup();
 
-while (true){
-    blink_LED();
+    while (true) {
+        ;
+    }
 }
 
-
-}
-
-void tim6_dac_isr () {
-    gpio_toggle(GPIOE, GPIO11);
-
+void tim6_dac_isr() {
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO9);
 }
